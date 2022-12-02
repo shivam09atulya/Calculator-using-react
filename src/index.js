a130c1882f2e32f058a4c1dae94ca19c2e7b5fcf
Shivam Atulya <shivamatulya09@gmail.com>
2022-12-03 01:22:30 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+// const root = ReactDOM.render(<App />, document.getElementById("root"));
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+ReactDOM.render(<App />, document.getElementById("root"));
